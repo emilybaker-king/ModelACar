1ee84b2188abed0b19691fd41f9610bed7e72019
Emily Baker-King <emilybaker-king@interapts-macbook-pro-102.local>
2018-08-20 14:32:03 -0500

    Initial Commit

---
+++ ModelACar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ModelACar
+//
+//  Created by Emily Baker-King on 8/20/18.
+//  Copyright © 2018 Emily Baker-King. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
